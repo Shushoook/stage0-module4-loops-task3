712329674042b16ff5d29ba45d423b66d504bec2
Shushoook <shushoook666@gmail.com>
2022-07-15 12:34:03 +0400

    stage0-module4-loops-task3

--- src/main/java/school/mjc/stage0/loops/task3/AlphabetPrinter.java
+++ src/main/java/school/mjc/stage0/loops/task3/AlphabetPrinter.java
@@ -2,5 +2,13 @@ package school.mjc.stage0.loops.task3;
 
 public class AlphabetPrinter {
     public void printAlphabet() {
+        char ch;
+        for( ch = 'A' ; ch <= 'Z' ; ch++ )
+            System.out.println(ch);
+    }
+
+    public static void main(String[] args) {
+        AlphabetPrinter a = new AlphabetPrinter();
+        a.printAlphabet();
     }
 }
